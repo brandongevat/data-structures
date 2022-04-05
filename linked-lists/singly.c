646eedc54ed3cff094b20ca7009ffca528ea2c90
brandongevat <brandongevat@gmail.com>
2022-04-05 14:38:04 -0400

    Convert to double pointers and refactor.

--- linked-lists/singly.c
+++ linked-lists/singly.c
@@ -1,146 +1,168 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-    int data;
-    struct node * next;
+typedef struct node
+{
+	int data;
+	struct node *next;
 } node;
 
-void displayLL(node*head);
+void displayLL(node *head);
 
-node * createNode(int data);
+node *createNode(int value);
 
-node * insertFront(node* head, int data);
+void insertFront(node **head, int value);
 
-node * insertEnd(node * head, int data);
+void insertEnd(node **head, int value);
 
-node * deleteNode(node * head, int data);
+void deleteNode(node **head, int value);
 
-node * recursiveDelete(node * head, int data);
+node *recursiveDelete(node **head, int value);
 
-node * insertSort(node * head, int data);
+void insertSort(node **head, int value);
 
-int main() {
-	node * head = malloc(sizeof(node));
+int main()
+{
+	node *head = malloc(sizeof(node));
 	head->data = 0;
 	head->next = NULL;
 	displayLL(head);
-	head = insertFront(head, 5);
+	insertFront(&head, 5);
 	displayLL(head);
-	head = insertEnd(head, 10);
+	insertEnd(&head, 10);
 	displayLL(head);
-	head = recursiveDelete(head, 5);
+	recursiveDelete(&head, 5);
+	recursiveDelete(&head, 10);
 	displayLL(head);
-	head = insertSort(head, 5);
+	insertSort(&head, 5);
 	displayLL(head);
 }
 
-
-node * insertSort(node * head, int data) {
-
-	node * newNode;
-	node * tmp;
-
-	newNode = (node *) malloc(sizeof(node));
-	newNode->data = data;
-	newNode->next = NULL;
-
-	if (head == NULL | head->data >= data) {
-		newNode->next = head;
-		head = newNode;
-	} else {
-		tmp = head;
-		while (tmp->next != NULL && tmp->next->data < data)
-			tmp = tmp->next;
-		newNode->next = tmp->next;
-		tmp->next = newNode;
-	}
-	return head;
-}
-
-
-node * recursiveDelete(node * head, int data) {
-	if (head == NULL)
-		return head;
-
-	if (head->data == data) {
-		node *tmp;
-		tmp = head;
-		head = head->next;
-		free(tmp);
-		return head;
-	}
-	head->next = recursiveDelete(head->next, data);
-	return head;
-}
-
-node * createNode(int data) {
-	node * tmp = malloc(sizeof(node));
-	tmp->data = data;
+node *createNode(int value)
+{
+	node *tmp = malloc(sizeof(node));
+	tmp->data = value;
 	tmp->next = NULL;
 	return tmp;
 }
 
-node * insertFront(node* head, int data) {
-	node * newNode = createNode(data);		
-	if (head == NULL)
-		head = newNode;
-	else {
-		newNode->next = head;
-		head = newNode;
+void insertFront(node **head, int value)
+{
+	node *newNode = createNode(value);
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		newNode->next = *head;
+		*head = newNode;
 	}
-	return head;
+	printf("%d inserted at the front.\n", value);
 }
 
-node * insertEnd(node * head, int data) {
-	node * tmp = head;
-	node * newNode = malloc(sizeof(node));
-	newNode->data = data;
+void insertSort(node **head, int value)
+{
+
+	node *newNode;
+	node *tmp;
+
+	newNode = (node *)malloc(sizeof(node));
+	newNode->data = value;
 	newNode->next = NULL;
-	if (head == NULL)
-		head = tmp;
-	else {
-		while(tmp->next != NULL) {
-		    tmp = tmp->next;
+
+	if (*head == NULL | (*head)->data >= value)
+	{
+		newNode->next = *head;
+		*head = newNode;
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL && tmp->next->data < value)
+			tmp = tmp->next;
+		newNode->next = tmp->next;
+		tmp->next = newNode;
+	}
+	printf("%d inserted at it's respective location.\n", value);
+}
+
+node *recursiveDelete(node **head, int value)
+{
+	if (*head == NULL)
+		return *head;
+
+	if ((*head)->data == value)
+	{
+		node *tmp;
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		printf("%d deleted recursively.\n", value);
+		return *head;
+	}
+	(*head)->next = recursiveDelete(&((*head)->next), value);
+	return *head;
+}
+
+void insertEnd(node **head, int value)
+{
+	node *tmp = *head;
+	node *newNode = malloc(sizeof(node));
+	newNode->data = value;
+	newNode->next = NULL;
+	if (*head == NULL)
+		*head = tmp;
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
 		}
 		tmp->next = newNode;
 	}
-	return head;
+	printf("%d inserted at the end.\n", value);
 }
 
-void displayLL(node*head) {
- 
-    node * tmp = head;
-    if (head == NULL)
-        printf("Linked list is empty.\n");
-    else {
-        printf("%d->", tmp->data);
-        while (tmp->next != NULL) {
-            printf("%d->", tmp->next->data);
-            tmp = tmp->next;
-        }
-        printf("NULL\n");
-    }
+void displayLL(node *head)
+{
+
+	node *tmp = head;
+	if (head == NULL)
+		printf("Linked list is empty. Cannot display.\n");
+	else
+	{
+		printf("%d->", tmp->data);
+		while (tmp->next != NULL)
+		{
+			printf("%d->", tmp->next->data);
+			tmp = tmp->next;
+		}
+		printf("NULL\n");
+	}
 }
 
-node * deleteNode(node * head, int data) {
-    node * prev = NULL;	
-    node * tmp = head;
-    if (head == NULL)
-        printf("List is empty\n");
-    while (tmp->next != NULL) {
-        if (tmp->data == data) {
-            if(prev != NULL) {
-                prev->next = tmp->next;
-	        head = prev;
-	    }
-	    else head = tmp->next;
-            free(tmp);
-            printf("%d deleted.\n", data); 
-            return head;
-        }
-	prev = tmp;
-	tmp = tmp->next;
-    }
-    printf("Node with value %d not found.\n", data);
+void deleteNode(node **head, int value)
+{
+	node *prev = NULL;
+	node *tmp = *head;
+	if (*head == NULL)
+		printf("LL is empty. Cannot delete %d.\n", value);
+	while (tmp->next != NULL)
+	{
+		if (tmp->data == value)
+		{
+			if (prev != NULL)
+			{
+				prev->next = tmp->next;
+				*head = prev;
+			}
+			else
+				*head = tmp->next;
+			free(tmp);
+			printf("%d deleted.\n", value);
+			return;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	printf("Node with value %d not found.\n", value);
 }
